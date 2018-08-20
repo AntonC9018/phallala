70ae58b73d92b2f0c2c237591b1acb5d9b0e0141
AntonC9018 <anton.curm@gmail.com>
2018-08-20 21:38:33 +0300

    oops one more bug

--- links/loadStrings.js
+++ links/loadStrings.js
@@ -1,22 +1,26 @@
 $(document).ready(function() {
-    $.ajax({
-        type: "GET",
-        url: "text.csv",
-        dataType: "text",
-        success: function(data) {processData(data);}
-     });
+  $.ajax({
+    type: "GET",
+    url: "text.csv",
+    dataType: "text",
+    success: function(data) {processData(data);}
+  });
 });
 
 function processData(allText) {
-    var allTextLines = allText.split(/\r\n|\n/);
-    var headers = allTextLines[0].split(',');
-    var lines = [];
+  var allTextLines = allText.split(/\r\n|\n/);
+  var headers = allTextLines[0].split(',');
+  var lines = [];
 
-    for (var i=1; i<allTextLines.length; i++) {
-        var data = allTextLines[i].split(',');
-        if (data.length === 1) break;
-        $(data[0]+" ol").append(
-          $('<li><a target="_blank" href="' + data[2] + '">'
-          + data[1] + '</a></li>'));
-        }
+  for (var i=1; i<allTextLines.length; i++) {
+    var data = allTextLines[i].split(',');
+    if (data.length === 1) break;
+    $(data[0]+" ol").append(
+      $('<li><a target="_blank" href="' + data[2] + '">'
+      + data[1] + '</a></li>'));
+    }
+  $("ol:not(:has(*))")
+  .parent()
+  .filter(".table")
+  .hide();
 }
