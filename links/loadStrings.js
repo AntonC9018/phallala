40e97d2446f34fc423b21653465826b173d14c20
AntonC9018 <anton.curm@gmail.com>
2018-08-20 22:32:21 +0300

    quick refactoring

--- links/loadStrings.js
+++ links/loadStrings.js
@@ -9,8 +9,6 @@ $(document).ready(function() {
 
 function processData(allText) {
   var allTextLines = allText.split(/\r\n|\n/);
-  var headers = allTextLines[0].split(',');
-  var lines = [];
 
   for (var i=1; i<allTextLines.length; i++) {
     var data = allTextLines[i].split(',');
